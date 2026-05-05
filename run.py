e89b7de78b598726991bf747d3aee0a4e6d570f3
arterafael21 <arterafael21@gmail.com>
2026-05-04 20:15:14 -0400

    added ssl context for https certificate to run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 """
 Entry point for Flask app
 Run with: python3 run.py
-Opens at: http://127.0.0.1:5000/search
+Opens at: https://127.0.0.1:5002/search
 """
 
 from app import create_app, socketio
@@ -9,4 +9,11 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, port=5002)
+    socketio.run(
+        app,
+        host="127.0.0.1",
+        port=5002,
+        ssl_context=("localhost+2.pem", "localhost+2-key.pem"),
+        debug=True,
+        allow_unsafe_werkzeug=True,
+    )
